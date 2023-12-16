01ad14d9828b57fb59e4bc2a279e63b08c19ed41
Aditya <sarnobatadi@gmail.com>
2023-12-16 19:45:22 +0530

    Updated test file as per standard Java Convention & Changes in dependencies

--- src/test/powertest.java
+++ src/test/powertest.java
@@ -1,26 +1,29 @@
 package powerpackage;
 
-
 import org.junit.Test;
 
 public class powertest {
     @Test
-    public void one_raised_to_one_is_onew() {
+    public void OnePowerOneIsOne() {
         assert powerfinder.OF(1,1) == 1;
     }
 
     @Test
-    public void twoRaised_to_one_is_2() {
+    public void TwoPowerOneIsTwo() {
+
         assert powerfinder.OF(2,1) == 2;
+
     }
 
     @Test
-    public void two_power_2_is_4() {
+    public void TwoPowerTwoIsFour() {
+
         assert powerfinder.OF(2, 2) == 4;
     }
 
     @Test
-    public void power_of_2_and_3_Is_SIX() {
+    public void ThreePowerTwoIsNine() {
+
         assert powerfinder.OF(3, 2) == 3*3;
     }
 }
